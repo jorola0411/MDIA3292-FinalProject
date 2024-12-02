71934e184822a9f05c14a9e1bac24ce287a7bcec
jorola0411 <jorola@my.bcit.ca>
2024-12-01 19:38:33 -0800

    wireframe design created

---
+++ artisan/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./login.html", "./product.html","./featured.html" ,"./artist.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
